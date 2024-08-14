8f1b1b7f4ab4a79b9daebdc574b23d2f44eef9e5
Simon <simon@simont.de>
2024-08-15 01:52:23 +0200

    tweaks

--- make_builds.bat
+++ make_builds.bat
@@ -1,9 +1,9 @@
 set godot_path="C:\Program Files\Godot_v4.2.2\Godot_v4.2.2-stable_win64.exe"
 set project_path=E:\private\projects\Godot\dlc_test\godot
 set export_path=E:\private\projects\Godot\dlc_test\builds\
-set export_path_dlc=dlcs\
 set export_path_file=dlc_test.exe
 
+set export_path_dlc=dlcs\
 set dlc_filetype=.zip
 set dlc_name_00=dlc_default
 set dlc_name_01=dlc_corgi
